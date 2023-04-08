497a5981ad971e959a734c65622ff75fd0a4bfa2
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 01:22:41 +0200

    Edited onBlockInteractListener

--- src/main/java/eu/zentomomc/mc2dc/Listeners/onBlockInteractListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/onBlockInteractListener.java
@@ -4,8 +4,6 @@ import eu.zentomomc.mc2dc.Mc2dc;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,8 +12,8 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class onBlockInteractListener implements Listener {
@@ -26,7 +24,7 @@ public class onBlockInteractListener implements Listener {
      *
      */
 
-    private final Map<UUID, ArmorStand> sittingPlayers = new HashMap<>();
+    private final Set<UUID> sitting = new HashSet<>();
 
     @EventHandler
     public void onBlockInteract(PlayerInteractEvent event) {
@@ -37,29 +35,18 @@ public class onBlockInteractListener implements Listener {
             if (block.getType().toString().contains("_STAIRS") || block.getType().toString().contains("_SLAB")) {
                 if (player.getGameMode() != GameMode.ADVENTURE && player.getGameMode() != GameMode.SPECTATOR) {
                     if (player.isSneaking()) {
-                        if (sittingPlayers.containsKey(player.getUniqueId())) {
-                            // Remove the sitting armor stand and update the player's location
-                            sittingPlayers.get(player.getUniqueId()).remove();
-                            sittingPlayers.remove(player.getUniqueId());
-                            player.teleport(player.getLocation().add(0, 1, 0));
+                        if (sitting.contains(player.getUniqueId())) {
+                            player.setVelocity(new Vector(0, -0.1, 0));
+                            sitting.remove(player.getUniqueId());
                             player.sendMessage("You are no longer sitting!");
                         } else {
-                            // Spawn a new armor stand and position it under the player
-                            ArmorStand armorStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation().subtract(0, 1, 0), EntityType.ARMOR_STAND);
-                            armorStand.setGravity(false);
-                            armorStand.setVisible(false);
-                            armorStand.setSmall(true);
-                            armorStand.setMarker(true);
-                            armorStand.addPassenger(player);
-                            sittingPlayers.put(player.getUniqueId(), armorStand);
+                            sitting.add(player.getUniqueId());
                             player.sendMessage("You are now sitting!");
-
-                            // Schedule a task to update the player's location to make it look like they are sitting
                             new BukkitRunnable() {
                                 @Override
                                 public void run() {
-                                    if (sittingPlayers.containsKey(player.getUniqueId())) {
-                                        player.teleport(sittingPlayers.get(player.getUniqueId()).getLocation().add(0, 1, 0));
+                                    if (sitting.contains(player.getUniqueId())) {
+                                        player.setVelocity(new Vector(0, -0.1, 0));
                                     } else {
                                         cancel();
                                     }
