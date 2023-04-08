f856a17b00134b613ae1c68ea7ab2163ef973797
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 01:03:31 +0200

    Edited onBlockInteractListener

--- src/main/java/eu/zentomomc/mc2dc/Listeners/onBlockInteractListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/onBlockInteractListener.java
@@ -18,62 +18,46 @@ import java.util.UUID;
 
 public class onBlockInteractListener implements Listener {
 
-    private final Set<UUID> sittingPlayers = new HashSet<>();
+    /**
+     * This class should let a Player on a Right Click Interaction with any Stairs
+     * or slabs, be able to sit on them.
+     *
+     */
+
+    private final Set<UUID> sitting = new HashSet<>();
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            Block clickedBlock = event.getClickedBlock();
-            if (clickedBlock != null && clickedBlock.getType() == Material.OAK_STAIRS) {
-                Player player = event.getPlayer();
-                if (sittingPlayers.contains(player.getUniqueId())) {
-                    // Player is already sitting, so stand them up
-                    standUp(player);
+    public void onBlockInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && block != null) {
+            if (block.getType().toString().contains("_STAIRS") || block.getType().toString().contains("_SLAB")) {
+                if (player.getGameMode() != GameMode.ADVENTURE && player.getGameMode() != GameMode.SPECTATOR) {
+                    if (player.isSneaking()) {
+                        if (sitting.contains(player.getUniqueId())) {
+                            player.setVelocity(new Vector(0, 0.5, 0));
+                            sitting.remove(player.getUniqueId());
+                            player.sendMessage("You are no longer sitting!");
+                        } else {
+                            sitting.add(player.getUniqueId());
+                            player.sendMessage("You are now sitting!");
+                            new BukkitRunnable() {
+                                @Override
+                                public void run() {
+                                    if (sitting.contains(player.getUniqueId())) {
+                                        player.setVelocity(new Vector(0, -0.5, 0));
+                                    } else {
+                                        cancel();
+                                    }
+                                }
+                            }.runTaskTimer(Mc2dc.getInstance(), 0, 1);
+                        }
+                    }
                 } else {
-                    // Player is not sitting, so sit them down
-                    sitDown(player, clickedBlock);
+                    player.sendMessage("You can't sit while in adventure mode or spectator mode!");
                 }
             }
         }
-    }
-
-    private void sitDown(Player player, Block stairBlock) {
-        if (player.getLocation().getBlock().getRelative(0, -1, 0).getType().isSolid()) {
-            // Calculate seat location based on player's facing direction
-            Vector facingDirection = player.getLocation().getDirection().normalize().setY(0);
-            Vector seatOffset = facingDirection.multiply(0.5).setY(0.5);
-            Vector seatLocation = stairBlock.getLocation().toVector().add(seatOffset);
-
-            // Set player's sitting status and prevent movement
-            sittingPlayers.add(player.getUniqueId());
-            player.setSneaking(true);
-            player.setVelocity(new Vector(0, 0, 0));
-            player.teleport(seatLocation.toLocation(stairBlock.getWorld()));
-            player.setFallDistance(0);
-            player.setGameMode(GameMode.ADVENTURE);
-
-            // Schedule task to periodically check if player is still sitting
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    if (!sittingPlayers.contains(player.getUniqueId())) {
-                        // Player is no longer sitting, so cancel task
-                        cancel();
-                    } else if (player.getLocation().getBlock().getType() != Material.OAK_STAIRS || !player.getLocation().getBlock().getRelative(0, -1, 0).getType().isSolid()) {
-                        // Player has moved off the stair block or is no longer standing on a solid block, so stand them up and cancel task
-                        standUp(player);
-                        cancel();
-                    }
-                }
-            }.runTaskTimer(Mc2dc.getInstance(), 0L, 1L);
-        }
-    }
-
-    private void standUp(Player player) {
-        // Set player's standing status and allow movement
-        sittingPlayers.remove(player.getUniqueId());
-        player.setSneaking(false);
-        player.setVelocity(player.getLocation().getDirection().normalize().multiply(0.5));
-        player.setGameMode(GameMode.SURVIVAL);
     }
 }
