c8f1ea37155cf8eec6dbf1f1f1be7247adca7c56
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 00:43:39 +0200

    Edited onBlockInteractListener

--- src/main/java/eu/zentomomc/mc2dc/Listeners/onBlockInteractListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/onBlockInteractListener.java
@@ -1,6 +1,7 @@
 package eu.zentomomc.mc2dc.Listeners;
 
 import eu.zentomomc.mc2dc.Mc2dc;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -37,31 +38,35 @@ public class onBlockInteractListener implements Listener {
     }
 
     private void sitDown(Player player, Block stairBlock) {
-        // Calculate seat location based on player's facing direction
-        Vector facingDirection = player.getLocation().getDirection().normalize().setY(0);
-        Vector seatOffset = facingDirection.multiply(0.5).setY(0.5);
-        Vector seatLocation = stairBlock.getLocation().toVector().add(seatOffset);
+        if (player.getLocation().getBlock().getRelative(0, -1, 0).getType().isSolid()) {
+            // Calculate seat location based on player's facing direction
+            Vector facingDirection = player.getLocation().getDirection().normalize().setY(0);
+            Vector seatOffset = facingDirection.multiply(0.5).setY(0.5);
+            Vector seatLocation = stairBlock.getLocation().toVector().add(seatOffset);
 
-        // Set player's sitting status and prevent movement
-        sittingPlayers.add(player.getUniqueId());
-        player.setSneaking(true);
-        player.setVelocity(new Vector(0, 0, 0));
-        player.teleport(seatLocation.toLocation(stairBlock.getWorld()));
+            // Set player's sitting status and prevent movement
+            sittingPlayers.add(player.getUniqueId());
+            player.setSneaking(true);
+            player.setVelocity(new Vector(0, 0, 0));
+            player.teleport(seatLocation.toLocation(stairBlock.getWorld()));
+            player.setFallDistance(0);
+            player.setGameMode(GameMode.ADVENTURE);
 
-        // Schedule task to periodically check if player is still sitting
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (!sittingPlayers.contains(player.getUniqueId())) {
-                    // Player is no longer sitting, so cancel task
-                    cancel();
-                } else if (player.getLocation().getBlock().getType() != Material.OAK_STAIRS) {
-                    // Player has moved off the stair block, so stand them up and cancel task
-                    standUp(player);
-                    cancel();
+            // Schedule task to periodically check if player is still sitting
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (!sittingPlayers.contains(player.getUniqueId())) {
+                        // Player is no longer sitting, so cancel task
+                        cancel();
+                    } else if (player.getLocation().getBlock().getType() != Material.OAK_STAIRS || !player.getLocation().getBlock().getRelative(0, -1, 0).getType().isSolid()) {
+                        // Player has moved off the stair block or is no longer standing on a solid block, so stand them up and cancel task
+                        standUp(player);
+                        cancel();
+                    }
                 }
-            }
-        }.runTaskTimer(Mc2dc.getInstance(), 0L, 1L);
+            }.runTaskTimer(Mc2dc.getInstance(), 0L, 1L);
+        }
     }
 
     private void standUp(Player player) {
@@ -69,7 +74,6 @@ public class onBlockInteractListener implements Listener {
         sittingPlayers.remove(player.getUniqueId());
         player.setSneaking(false);
         player.setVelocity(player.getLocation().getDirection().normalize().multiply(0.5));
+        player.setGameMode(GameMode.SURVIVAL);
     }
 }
-
-
