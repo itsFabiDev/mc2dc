e837c71bc05b9f25a7196a6c2140cf459f546ff7
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 15:19:35 +0200

    Added a SittingListener

--- src/main/java/eu/zentomomc/mc2dc/Listeners/SittingListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/SittingListener.java
@@ -1,8 +1,6 @@
 package eu.zentomomc.mc2dc.Listeners;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,42 +10,48 @@ import org.bukkit.util.Vector;
 
 public class SittingListener implements Listener {
     private static boolean isSitting = false;
+    private static Location sitLocation;
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if (event.getAction().toString().contains("RIGHT_CLICK")
                 && event.getClickedBlock() != null
-                && event.getClickedBlock().getType().toString().contains("STAIRS")) {
-            isSitting = true;
-            player.setSneaking(true);
-            player.setVelocity(new Vector(0, 0, 0));
-            player.setGravity(false);
-            player.setAllowFlight(true);
-            player.setFlying(true);
-            player.sendTitle("", "§c§lSitting", 0, 20, 0);
-            player.teleport(player.getLocation().add(0.5, -0.25, 0.5));
-            event.setCancelled(true);
+                && event.getClickedBlock().getType().name().endsWith("_STAIRS")) {
+            if (!isSitting) {
+                isSitting = true;
+                player.setSneaking(true);
+                player.setVelocity(new Vector(0, 0, 0));
+                player.setGravity(false);
+                player.setAllowFlight(true);
+                player.setFlying(true);
+                player.sendTitle("", "§c§lSitting", 0, 20, 0);
+                sitLocation = player.getLocation();
+                sitLocation.setYaw(player.getLocation().getYaw());
+                sitLocation.setPitch(0);
+                sitLocation.add(0.5, -0.25, 0.5);
+                player.teleport(sitLocation);
+                event.setCancelled(true);
+            }
         }
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (isSitting) {
-            Player player = event.getPlayer();
-            Location location = player.getLocation();
-            if (player.isSneaking()) {
-                if (!location.getBlock().getType().toString().contains("STAIRS")) {
-                    isSitting = false;
-                    player.setSneaking(false);
-                    player.setGravity(true);
-                    player.setAllowFlight(false);
-                    player.setFlying(false);
-                    player.sendTitle("", "§a§lStanding up", 0, 20, 0);
-                    player.teleport(location.add(0, 1, 0));
-                    event.setCancelled(true);
-                }
+        if (isSitting && event.getPlayer().isSneaking()) {
+            Location location = event.getPlayer().getLocation();
+            if (location.getBlock().getType().name().endsWith("_STAIRS")
+                    && location.getBlock().getLocation().equals(sitLocation.getBlock().getLocation())) {
+                return;
             }
+            isSitting = false;
+            event.getPlayer().setSneaking(false);
+            event.getPlayer().setGravity(true);
+            event.getPlayer().setAllowFlight(false);
+            event.getPlayer().setFlying(false);
+            event.getPlayer().sendTitle("", "§a§lStanding up", 0, 20, 0);
+            event.getPlayer().teleport(sitLocation.add(0, 1, 0));
+            event.setCancelled(true);
         }
     }
 }
