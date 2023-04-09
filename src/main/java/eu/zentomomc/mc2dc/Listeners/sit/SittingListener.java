a817c136c76044a207918dc4b6ecff73ae3f66f9
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 18:43:46 +0200

    Edited the SittingListener and added a ServerRestartListener

--- src/main/java/eu/zentomomc/mc2dc/Listeners/sit/SittingListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/sit/SittingListener.java
@@ -39,7 +39,10 @@ public class SittingListener implements Listener {
         Player player = event.getPlayer();
         SittingData sittingData = SittingData.getSittingData(player);
         if (sittingData != null) {
-            if (player.isSneaking()) {
+            if(player.getLocation().getY() < sittingData.getSittingLocation().getY() || player.getLocation().getY() > sittingData.getSittingLocation().getY()) {
+                player.teleport(sittingData.getSittingLocation());
+                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+            } else if (player.isSneaking()) {
                 Block blockBelow = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
                 if (sittingData.isSittingOn(blockBelow.getLocation())) {
                     sittingData.updateSittingPosition();
@@ -49,10 +52,7 @@ public class SittingListener implements Listener {
             } else {
                 event.setCancelled(true);
             }
-            if(player.getLocation().getY() < sittingData.getSittingLocation().getY() || player.getLocation().getY() > sittingData.getSittingLocation().getY()) {
-                player.teleport(sittingData.getSittingLocation());
-                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
-            }
+
         }
     }
 }
