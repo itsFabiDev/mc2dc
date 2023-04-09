88a439e739dc59e574ae952f03e26a1190644274
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 12:50:27 +0200

    Try and Error

--- src/main/java/eu/zentomomc/mc2dc/discord/DiscordVerifier.java
+++ src/main/java/eu/zentomomc/mc2dc/discord/DiscordVerifier.java
@@ -28,7 +28,7 @@ public class DiscordVerifier {
             Bukkit.getConsoleSender().sendMessage("Could not find guild!");
             return false;
         }
-        Bukkit.getConsoleSender().sendMessage("Guild: " + guild.getName());
+        Bukkit.getConsoleSender().sendMessage("Guild: " + guild.getName().toString());
         Member member = guild.getMemberById(discordID);
         Bukkit.getConsoleSender().sendMessage("Member: " + member.getNickname());
         if (member == null) {
