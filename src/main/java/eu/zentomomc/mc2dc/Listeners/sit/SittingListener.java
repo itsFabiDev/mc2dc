cfab5ecf30513fad0ad466d4978633f3c6828869
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 18:42:16 +0200

    Edited the SittingListener and added a ServerRestartListener

--- src/main/java/eu/zentomomc/mc2dc/Listeners/sit/SittingListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/sit/SittingListener.java
@@ -43,7 +43,11 @@ public class SittingListener implements Listener {
                 Block blockBelow = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
                 if (sittingData.isSittingOn(blockBelow.getLocation())) {
                     sittingData.updateSittingPosition();
+                } else {
+                    sittingData.stopSitting();
                 }
+            } else {
+                event.setCancelled(true);
             }
             if(player.getLocation().getY() < sittingData.getSittingLocation().getY() || player.getLocation().getY() > sittingData.getSittingLocation().getY()) {
                 player.teleport(sittingData.getSittingLocation());
