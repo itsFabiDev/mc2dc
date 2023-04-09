3d0b46574c11991f5a511800844e701391e9ba6c
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-09 14:35:15 +0200

    Edited the Message Listeners

--- src/main/java/eu/zentomomc/mc2dc/Listeners/QuitListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/QuitListener.java
@@ -3,10 +3,12 @@ package eu.zentomomc.mc2dc.Listeners;
 import eu.zentomomc.mc2dc.discord.discordbot;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class QuitListener implements Listener {
+    @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         String dcmessage = player.getName() + " has left the server!";
--- src/main/java/eu/zentomomc/mc2dc/Listeners/onDeathListener.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/onDeathListener.java
@@ -3,10 +3,12 @@ package eu.zentomomc.mc2dc.Listeners;
 import eu.zentomomc.mc2dc.discord.discordbot;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class onDeathListener implements Listener {
+    @EventHandler
     public void onDeath(PlayerDeathEvent event) {
         Player player = event.getPlayer();
         String dcmessage = player.getName() + " has died!";
