175d72968a57b7d4acffb1ef5e8c2ce773ec79f2
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-10 20:52:28 +0200

    Edited SittingData

--- src/main/java/eu/zentomomc/mc2dc/Listeners/sit/SittingData.java
+++ src/main/java/eu/zentomomc/mc2dc/Listeners/sit/SittingData.java
@@ -60,11 +60,11 @@ public class SittingData {
             player.setAllowFlight(false);
             player.setFlying(false);
             player.sendTitle("", "§a§lStanding up", 0, 20, 0);
-            horse.remove();
             isSitting = false;
             player.teleport(tpBack);
             player.sendMessage(horse.getUniqueId());
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kill @e[name=" + player.getName() + ",type=minecraft:horse]");
+            horse.remove();
+            //Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kill @e[name=" + player.getName() + ",type=minecraft:horse]");
             sittingPlayers.remove(player);
         }
     }
