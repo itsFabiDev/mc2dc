04c900817f75a41d05838c5cbf11c0eed3573913
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-29 09:46:37 +0200

    Added an AnvilListener to hopefully make it possible to combine an Diamond Axe with an Book of Looting

--- src/main/java/eu/zentomomc/mc2dc/Mc2dc.java
+++ src/main/java/eu/zentomomc/mc2dc/Mc2dc.java
@@ -44,7 +44,7 @@ public final class Mc2dc extends JavaPlugin {
         registerListener(new SittingListener());
         registerListener(new ServerRestartListener());
         registerListener(new onPlayerMove());
-        registerListener(new onPrepareAnvilListener());
+        //registerListener(new onPrepareAnvilListener());
         registerCommand(new TPACommand(), "tpa");
         registerCommand(new DiscordStopCommand(),"stopdc");
         registerCommand(new DiscordStartCommand(),"startdc");
