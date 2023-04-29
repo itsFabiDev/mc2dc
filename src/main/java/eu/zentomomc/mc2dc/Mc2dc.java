1475313e4b6af428ae25249a2b66f03d6dd9024f
Itsfabidev <fabianp.mueller04@gmail.com>
2023-04-29 09:47:14 +0200

    Doesnt work :(

--- src/main/java/eu/zentomomc/mc2dc/Mc2dc.java
+++ src/main/java/eu/zentomomc/mc2dc/Mc2dc.java
@@ -45,6 +45,7 @@ public final class Mc2dc extends JavaPlugin {
         registerListener(new ServerRestartListener());
         registerListener(new onPlayerMove());
         //registerListener(new onPrepareAnvilListener());
+        //Doesnt work
         registerCommand(new TPACommand(), "tpa");
         registerCommand(new DiscordStopCommand(),"stopdc");
         registerCommand(new DiscordStartCommand(),"startdc");
